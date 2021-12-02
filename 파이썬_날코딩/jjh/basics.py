cb75ec42fff13c28a67223c4101a38e0666cbe70
HKonly-KU <hkonly@korea.ac.kr>
2021-12-02 15:38:07 +0900

    final update

--- 파이썬_날코딩/jjh/basics.py
+++ 파이썬_날코딩/jjh/basics.py
@@ -9,9 +9,9 @@ class model:
         self.rnd_mean = kargs['rnd_mean']
         self.rnd_std = kargs['rnd_std']
 
-    def default_randomize(): np.random.seed(1234)
+    def default_randomize(self): np.random.seed(1234)
 
-    def randomize(): np.random.seed(time.time())
+    def randomize(self): np.random.seed(time.time())
 
     def load_abalone_dataset(self):
         with open('./abalone.csv') as csvfile:
@@ -32,9 +32,10 @@ class model:
         self.weight = np.random.normal(self.rnd_mean, self.rnd_std, [self.input_count, self.output_count])
         self.bias = np.zeros([self.output_count])
 
-    def train_and_test(self, epoch_count, mb_size, report):
+    def train_and_test(self, epoch_count, mb_size, report, learning_rate):
         step_count = self.arrange_data(mb_size)
         test_x, test_y = self.get_test_data()
+        self.learning_rate = learning_rate
 
         for epoch in range(epoch_count):
             losses, accs = [], []
@@ -53,7 +54,7 @@ class model:
         print('\nFinal Test: final accuracy = {:5.3f}'.format(final_acc))
 
     def arrange_data(self, mb_size):
-        self.shuffle_map = np.arrange(self.data.shape[0])
+        self.shuffle_map = np.arange(self.data.shape[0])
         np.random.shuffle(self.shuffle_map)
         step_count = int(self.data.shape[0] * 0.8) // mb_size
         self.test_begin_idx = step_count * mb_size
@@ -76,7 +77,7 @@ class model:
 
         G_loss = 1.0
         G_output = self.backprop_postproc(G_loss, aux_pp)
-        self.backprop_neuralnet(G_output, aux_nn)
+        self.backprop_neuralnet(G_output, aux_nn, self.learning_rate)
 
         return loss, accuracy
 
@@ -85,4 +86,37 @@ class model:
         accuracy = self.eval_accuracy(output, y)
         return accuracy
 
-    
+    def forward_neuralnet(self, x):
+        output = np.matmul(x, self.weight) + self.bias
+        return output, x
+
+    def backprop_neuralnet(self, G_output, x, learning_rate):
+        g_output_w = x.transpose()
+
+        G_w = np.matmul(g_output_w, G_output)
+        G_b = np.sum(G_output, axis=0)
+
+        self.weight -= learning_rate * G_w
+        self.bias -= learning_rate * G_b
+
+    def forward_postproc(self, output, y):
+        diff = output - y
+        square = np.square(diff)
+        loss = np.mean(square)
+        return loss, diff
+
+    def backprop_postproc(self, G_loss, diff):
+        shape = diff.shape
+
+        g_loss_square = np.ones(shape) / np.prod(shape)
+        g_square_diff = 2 * diff
+        g_diff_output = 1
+
+        G_square = g_loss_square * G_loss
+        G_diff = g_square_diff * G_square
+        G_output = g_diff_output * G_diff
+        return G_output
+
+    def eval_accuracy(self, output, y):
+        mdiff = np.mean(np.abs((output-y)/y))
+        return 1 - mdiff
